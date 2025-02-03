a0bd58d9f6e554044549bde007e07ad19994c785
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-03 13:36:49 +0800

    user signup

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 const restController = require("../controllers/restaurant-controller");
 const adminController = require("./modules/admin");
+const userController = require("../controllers/user-controller");
 
 router.get("/restaurants", restController.getRestaurants);
-router.use("/admin",adminController);
+router.use("/admin", adminController);
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp);
 router.use("/", (req, res) => res.redirect("/restaurants"));
 
 module.exports = router;
