d86618d605bd601d8820df4e766d977644e37e22
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-03 16:03:50 +0800

    add header & footer

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3000;
 const flash = require("connect-flash");
 const session = require("express-session");
 const SESSION_SECRET = "secret";
-const passport = require('passport');
+const passport = require("passport");
+const { getUser } = require("./helpers/auth-helper");
+const handlebarsHelpers = require("./helpers/handlebars-helpers");
 
 app.use(
   session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
@@ -21,11 +23,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash("success_messages"); // 設定 success_msg 訊息
   res.locals.error_messages = req.flash("error_messages"); // 設定 warning_msg 訊息
+  res.locals.user = getUser(req);
   next();
 });
 
 app.use(routes);
-app.engine("hbs", handlebars({ extname: ".hbs" }));
+app.engine("hbs", handlebars({ extname: ".hbs", helpers: handlebarsHelpers }));
 app.set("view engine", "hbs");
 
 app.listen(port, () => {
