0a3e70d672b46dd8ebd3a79ff5b589c7717d5377
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-03 14:43:23 +0800

    flash msg & signup verification

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const routes = require("./routes");
 const handlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
+const flash = require('connect-flash');
+const session = require('express-session');
+const SESSION_SECRET = 'secret'
 
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) 
 app.use(express.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  next()
+})
+
 app.use(routes);
 app.engine("hbs", handlebars({ extname: ".hbs" }));
 app.set("view engine", "hbs");
