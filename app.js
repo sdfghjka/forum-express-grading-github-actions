47da6dbe177055707782ddfb0aa2f94805731929
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-06 21:49:19 +0800

    add user manage page

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
 app.use(methodOverride("_method"));
 app.use("/upload", express.static(path.join(__dirname, "upload")));
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../../controllers/admin-controller");
-const upload = require('../../middleware/multer'); 
+const upload = require("../../middleware/multer");
+const { ro } = require("faker/lib/locales");
 
 router.get("/restaurants", adminController.getRestaurants);
 router.get("/restaurants/create", adminController.createRestaurant);
@@ -18,5 +19,7 @@ router.post(
   upload.single("image"),
   adminController.postRestaurant
 );
+router.get("/users", adminController.getUsers);
+router.patch("/users/:id", adminController.patchUser);
 router.use("/", (req, res) => res.redirect("/admin/restaurants"));
 module.exports = router;
