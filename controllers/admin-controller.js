8f485dff3bf24abfaedc9dc98e391df196425e0c
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-07 16:12:52 +0800

    add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,21 +1,30 @@
-const { Restaurant } = require("../models");
+const { Restaurant, Category } = require("../models");
 const { localFileHandler } = require("../helpers/file-helpers");
 
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true,
+      nest: true,
+      include: [Category]
     })
 
       .then((restaurants) => res.render("admin/restaurants", { restaurants }))
 
       .catch((err) => next(err));
   },
-  createRestaurant: (req, res) => {
-    return res.render("admin/create-restaurant");
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({
+      raw: true,
+    })
+      .then((categories) =>
+        res.render("admin/create-restaurant", { categories })
+      )
+      .catch((err) => next(err));
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body;
+    const { name, tel, address, openingHours, description, categoryId } =
+      req.body;
     if (!name) throw new Error("Restaurant name is required!");
     //修改以下
     const { file } = req; // 把檔案取出來，也可以寫成 const file = req.file
@@ -29,6 +38,7 @@ const adminController = {
           openingHours,
           description,
           image: filePath || null,
+          categoryId,
         })
       )
       .then(() => {
@@ -40,6 +50,8 @@ const adminController = {
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
       raw: true,
+      nest: true,
+      include: [Category]
     })
       .then((restaurant) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!");
@@ -48,17 +60,19 @@ const adminController = {
       .catch((err) => next(err));
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
-      raw: true,
-    })
-      .then((restaurant) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!");
-        res.render("admin/edit-restaurant", { restaurant });
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true }),
+    ])
+      .then(([restaurant, categories]) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!");
+        res.render("admin/edit-restaurant", { restaurant, categories });
       })
       .catch((err) => next(err));
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body;
+    const { name, tel, address, openingHours, description, categoryId } =
+      req.body;
     if (!name) throw new Error("Restaurant name is required!");
     const { file } = req; // 把檔案取出來
     Promise.all([
@@ -77,6 +91,7 @@ const adminController = {
           openingHours,
           description,
           image: filePath || restaurant.image, // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
+          categoryId,
         });
       })
       .then(() => {
