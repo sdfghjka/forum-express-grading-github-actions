5f39c60a4d8bb6c2d9ff27c3c59b412d41b9a0cb
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-08 23:13:46 +0800

    add delete comment for admin

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,7 @@ router.post(
   userController.signIn
 );
 router.get("/logout", userController.logout);
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post("/comments", authenticated, commentController.postComment);
 router.use("/", (req, res) => res.redirect("/restaurants"));
 router.use("/", generalErrorHandler);
