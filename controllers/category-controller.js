0b80b2273473fbe2fc99ed1aa7d1ed890f9f74e2
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-08 13:08:23 +0800

    category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,23 +1,42 @@
 const { Category } = require("../models");
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({
-      raw: true,
-    })
-      .then((categories) => res.render("admin/categories", { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null,
+    ])
+      .then(([categories, category]) => {
+        res.render("admin/categories", {
+          categories,
+          category,
+        });
+      })
       .catch((err) => next(err));
   },
   postCategory: (req, res) => {
     const { name } = req.body;
-    if(!name) throw new Error("Category name is required!")
+    if (!name) throw new Error("Category name is required!");
     Category.create({
       name,
-    }).then(()=>{
-      req.flash('success_messages','新增類別成功!')
-      res.redirect("/admin/categories");
-    }).catch((error)=>{
-      next(error)
     })
+      .then(() => {
+        req.flash("success_messages", "新增類別成功!");
+        res.redirect("/admin/categories");
+      })
+      .catch((error) => {
+        next(error);
+      });
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body;
+    if (!name) throw new Error("Category name is required!");
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        if (!category) throw new Error("Category doesn't exist!");
+        return category.update({ name });
+      })
+      .then(() => res.redirect("/admin/categories"))
+      .catch((err) => next(err));
   },
 };
 module.exports = categoryController;
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -21,6 +21,8 @@ router.post(
 );
 router.get("/users", adminController.getUsers);
 router.patch("/users/:id", adminController.patchUser);
+router.get("/categories/:id", categoryController.getCategories);
+router.put("/categories/:id", categoryController.putCategory);
 router.get("/categories", categoryController.getCategories);
 router.post("/categories", categoryController.postCategory);
 router.use("/", (req, res) => res.redirect("/admin/restaurants"));
