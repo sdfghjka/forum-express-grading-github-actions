956222053f21478c662a32dd1eb63f1b42f5942b
sdfghjka <kxnenao100@yahoo.com.tw>
2025-02-10 21:19:12 +0800

    create followship model

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcryptjs");
-const { User, Restaurant} = require("../models");
+const { User, Restaurant } = require("../models");
 // set up Passport strategy
 passport.use(
   new LocalStrategy(
@@ -39,7 +39,12 @@ passport.serializeUser((user, cb) => {
 });
 passport.deserializeUser((id, cb) => {
   return User.findByPk(id, {
-    include: [{ model: Restaurant, as: "FavoritedRestaurants" },{ model: Restaurant, as: "LikeRestaurants" }],
+    include: [
+      { model: Restaurant, as: "FavoritedRestaurants" },
+      { model: Restaurant, as: "LikeRestaurants" },
+      { model: User, as: "Followers" }, // 新增這行
+      { model: User, as: "Followings" },
+    ],
   }).then((user) => {
     user = user.toJSON();
     return cb(null, user);
